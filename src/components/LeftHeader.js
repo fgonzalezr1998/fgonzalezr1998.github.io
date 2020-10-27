f83ece5ec0d5a6f8aac8be0f0db59a805b26ee40
fgonzalezr1998 <fergonzaramos@yahoo.es>
2020-10-27 01:18:22 +0100

    [Main structure] main structure of react added

---
+++ src/components/LeftHeader.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../styles/left_header.css"
+
+function LeftHeader() {
+  return(
+    <div id="left-header">
+
+      <p>Hello World</p>
+
+    </div>
+  )
+}
+
+export default LeftHeader;
---
+++ src/components/MainContent.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "../styles/left_header.css"
+
+function LeftHeader() {
+  return(
+    <div>
+      Hello World
+      <br /><br />
+      jejej
+    </div>
+  )
+}
+
+export default LeftHeader;
